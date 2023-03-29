0f5b3f5dbc9bd9dc4c3e680a747f0497a2685c03
ViniciusTei <viniciustprates@gmail.com>
2023-03-28 21:02:10 -0300

    fix file upload validation

--- src/middleware/upload.ts
+++ src/middleware/upload.ts
@@ -1,5 +1,18 @@
-import multer from 'multer'
+import multer, { FileFilterCallback } from 'multer'
+import { Request } from 'express'
 
-const upload = multer({ dest: 'uploads/' })
+function filter(req: Request, file: Express.Multer.File, cb: FileFilterCallback) {
+  if (file.mimetype == 'image/png' || file.mimetype == 'image/jpg' || file.mimetype == 'image/jpeg') {
+    cb(null, true)
+  } else if (file.size > 5000000) {
+    cb(null, false)
+    return cb(new Error('File too big. The must have 5MB maximum!'))
+  } else {
+    cb(null, false)
+    return cb(new Error('Only .png, .jpg and .jpeg format allowed!'))
+  }
+}
+
+const upload = multer({ dest: 'uploads/', fileFilter: filter })
 
 export default upload.single('photo')
