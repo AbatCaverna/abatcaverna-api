1e8d505d1ee82acd4f7b38c63c3f2778fc752aaf
ViniciusTei <viniciustprates@gmail.com>
2023-04-09 10:30:21 -0300

    feat add new user email

--- src/providers/job/definitions/email.ts
+++ src/providers/job/definitions/email.ts
@@ -3,6 +3,7 @@ import Handlers from '../handlers/email'
 
 const emailDefinitions = (agenda: Agenda) => {
   agenda.define('send.email.checkout', Handlers.sendCheckoutEmail)
+  agenda.define('send.email.user', Handlers.sendNewUserEmail)
 }
 
 export default emailDefinitions
--- src/providers/job/scheduler.ts
+++ src/providers/job/scheduler.ts
@@ -3,7 +3,16 @@ import { CheckouEmailData } from '../email'
 
 const schedule = {
   completeCheckout: async (data: CheckouEmailData) => {
+    console.log(
+      `[SERVER](${new Date().toDateString()}): Scheduler is sending email to ${data.to} in 1 minute`
+    )
     await agenda.schedule('in 1 minute', 'send.email.checkout', data)
+  },
+  newUser: async (email: string, name: string) => {
+    console.log(
+      `[SERVER](${new Date().toDateString()}): Scheduler is sending email to ${email} now`
+    )
+    await agenda.now('send.email.user', {email, name})
   }
 }
 
